d4acaf858a6e222c46f48acf7fb8afe1a680013f
Luka Pavlovic <lukap@yelp.com>
2021-10-19 14:22:32 +0200

    Adds packer

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,11 @@
-require('tools')
-
 local o = vim.o
 local wo = vim.wo
 local bo = vim.bo
+
+local map = vim.api.nvim_set_keymap
+
+-- set up packer
+require("plugins")
 
 -- global options
 o.swapfile = true
@@ -21,3 +24,17 @@ wo.wrap = false
 
 -- buffer-local options
 bo.expandtab = true
+bo.shiftwidth = 2
+bo.tabstop = 2
+
+-- mapping
+-- Example usage:
+--   map( mode, mappable key, behaviour, options )
+map('n', ',/', ':noh<CR>', {})
+
+-- commands
+cmds = {
+  [[ command! Scratch lua require('tools').makeScratch() ]]
+}
+vim.cmd(cmds[1])
+
